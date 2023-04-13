25ba73cce1e9aa0aafc5e3e52e5fe8ef43dfe051
respelarga <38775180+respelarga@users.noreply.github.com>
2023-04-13 17:33:00 +0800

    MP-1000 Fixed cors issue and add session id in context

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :key, presence: true, format: { with:  /\A^[a-zA-Z0-9]{12}\z/, message: "must be a valid 12 alphanumeric key" }
+    validates :key, presence: true, format: { with:  /\A^[a-zA-Z0-9]{12}\z/, message: "must be a valid 12 alphanumeric key" }, uniqueness: true
+
 end
