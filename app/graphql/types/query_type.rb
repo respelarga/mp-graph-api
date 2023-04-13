fe42c6d446603ee7dc81d8fe3feb5f27ad0a15f7
respelarga <38775180+respelarga@users.noreply.github.com>
2023-04-13 09:59:57 +0800

    MP-1000 Added basic graphql query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,5 +13,48 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :users, [Types::UserType], null: false
+
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false do 
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+
+    field :products, [Types::ProductType], null: false
+
+    def products
+      Product.all
+    end
+
+    field :product, Types::ProductType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def product(id:)
+      Product.find(id)
+    end
+
+    field :discount,[Types::DiscountType], null:false
+
+    def discount
+      Discount.all
+    end
+
+    field :cart, Types::CartType, null: false do
+      argument :session, String, required: true
+    end
+
+    def cart(session:)
+      Cart.find_by(session: session)
+    end
+
   end
 end
